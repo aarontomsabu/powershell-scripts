fdfbddc78ae617d58268a7fa09cd2fe35e924e91
aarontomsabu <aarontom412@gmail.com>
2026-07-03 13:35:10 +0530

    Update UserAccountManagement.ps1

--- UserAccountManagement.ps1
+++ UserAccountManagement.ps1
@@ -1,8 +1,20 @@
 Import-Csv "users.csv" | ForEach-Object {
-    New-ADUser `
-        -Name $_.Name `
-        -GivenName $_.FirstName `
-        -Surname $_.LastName `
-        -SamAccountName $_.Username `
-        -Enabled $true
+    try {
+        $securePassword = ConvertTo-SecureString "ChangeMe123!" -AsPlainText -Force
+
+        New-ADUser `
+            -Name $_.Name `
+            -GivenName $_.FirstName `
+            -Surname $_.LastName `
+            -SamAccountName $_.Username `
+            -UserPrincipalName "$($_.Username)@yourdomain.com" `
+            -AccountPassword $securePassword `
+            -Enabled $true `
+            -ChangePasswordAtLogon $true
+
+        Write-Host "Created user: $($_.Username)" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Failed to create user $($_.Username): $($_.Exception.Message)" -ForegroundColor Red
+    }
 }
